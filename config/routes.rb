7b622ef953125f53b727e8a12aa6993063d3b392
tosFa <hoti.fatos@gmail.com>
2016-11-18 17:07:58 +0100

    CRUD improve

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '', to: 'welcome#home'
-  get 'welcome/home', to: 'welcome#home'
-  get 'welcome/about', to: 'welcome#about'
+
 end
