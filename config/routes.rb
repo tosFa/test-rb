51955b8bd4f9f49c1e367250e533b379631dc94f
tosFa <hoti.fatos@gmail.com>
2016-11-21 16:06:03 +0100

    signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 	root to: 'welcome#home'
+	get 'signup', to: 'users#new'
+	resources :users, except: [ :new ]
+
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
