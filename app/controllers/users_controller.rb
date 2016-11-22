96bc51ceaac6d472a5b2d5b580736697466b85ad
tosFa <hoti.fatos@gmail.com>
2016-11-22 15:40:07 +0100

    signin after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
 
 		if @user.save
 			flash[:success] = "Success"
-			redirect_to articles_path
+			session[:user_id] = @user.id
+			redirect_to user_path(@user)
 		else
 			render 'new'
 		end
