e170f37077484979fdb88ef43ee46f332087ed31
tosFa <hoti.fatos@gmail.com>
2016-11-22 15:32:55 +0100

    auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 	before_action :set_user, only: [:edit, :update, :show]
-
+	before_action :require_same_user, only: [:edit, :update]
 	def new
 		@user = User.new
 	end
@@ -46,4 +46,11 @@ class UsersController < ApplicationController
 			@user = User.find(params[:id])
 		end
 
+		def require_same_user
+			if !logged_in? || current_user != @user
+				flash[:danger] = "You can only edit or delete your own account"
+				redirect_to root_path
+
+			end
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 	root to: 'welcome#home'
+
 	get 'signup', to: 'users#new'
 	resources :users, except: [ :new ]
 
   resources :articles
+
+  get 'login', to: 'sessions#new'
+	post 'login', to: 'sessions#create'
+	delete 'logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
